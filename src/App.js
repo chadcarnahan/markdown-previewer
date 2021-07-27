3875053f7cf0ee7ad1ab2e435bb7d07dd9cd046b
Chad Carnahan <chadcarnahan1@yahoo.com>
2021-07-27 16:00:12 -0500

    markdown-previewr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Container, Row, Col, textarea } from "react-bootstrap";
+import { useState } from "react";
+import marked from "marked";
+import { defaultText } from "./text.js";
+
+const App = () => {
+  const [text, setText] = useState(defaultText);
+  const [hidden, setHidden] = useState(false);
+  console.log(defaultText);
+  const show = (id, idToHide) => {
+    if (!hidden) {
+      id === "editor"
+        ? (document.getElementById(id).style.height = "600px")
+        : (document.getElementById(id).style.height = "auto");
+      document.getElementById(idToHide).style.display = "none";
+      setHidden(true);
+    } else {
+      id === "editor"
+        ? (document.getElementById(id).style.height = "200px")
+        : (document.getElementById(id).style.height = "auto");
+      document.getElementById(idToHide).style.display = "block";
+      setHidden(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      fluid
+      id="markdown"
+      style={{ width: "70%", margin: "auto", maxWidth: "700px" }}
+    >
+      <Row id="firstBox" style={{ marginTop: "20px" }}>
+        <Col id="header">
+          Editor
+          <i
+            onClick={() => show("editor", "whole")}
+            class="bi bi-arrows-angle-expand"
+          ></i>
+        </Col>
+        <textarea onChange={(e) => setText(e.target.value)} id="editor">
+          {defaultText}
+        </textarea>
+      </Row>
+      <Row id="whole" style={{ marginTop: "40px" }}>
+        <Col id="header2" style={{ width: "730px", height: "30px" }}>
+          Previewer
+          <i
+            onClick={() => show("preview", "firstBox")}
+            class="bi bi-arrows-angle-expand"
+          ></i>
+        </Col>
+        <Col
+          id="preview"
+          style={{
+            width: "700px",
+            height: "auto",
+            padding: "20px",
+            paddingTop: "5px",
+          }}
+          dangerouslySetInnerHTML={{
+            __html: marked(text, { sanitize: false }),
+          }}
+        ></Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/text.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-expressions
+export const defaultText = `
+# Welcome to my Markdown Previewer!
+
+## Some examples of what you can do are this subheading
+
+You can use [links](https:/www.freecodecamp.org)
+
+    > You can use blockquotes
+
+You can do some coding \`<div> </div>\` between backticks
+    use multiple backticks to do a code block
+    
+\`\`\`
+// this is multi-line code:
+const markdown = () => {
+    if (1 + 1 === 2){
+        return true
+    }
+}
+\`\`\`
+You can make words **bold**
+ 
+and you can also use images
+
+![dogImage](https://i.kym-cdn.com/photos/images/facebook/001/457/007/ed5.jpeg)
+
+`;
